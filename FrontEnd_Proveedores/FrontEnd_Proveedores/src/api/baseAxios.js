782d1b38c799dd127f4bb46686d093045c84e02f
rramirez37 <a20190302@pucp.edu.pe>
2025-08-15 08:42:29 -0500

    Final Update

--- FrontEnd_Proveedores/FrontEnd_Proveedores/src/api/baseAxios.js
+++ FrontEnd_Proveedores/FrontEnd_Proveedores/src/api/baseAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const supplierAPI = axios.create({
-    baseURL: `https://localhost:7014/api/`
+    baseURL: `http://localhost:5171/api/`
 })
 
 export const scrapeAPI = axios.create({
